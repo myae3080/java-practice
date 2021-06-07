448b1cce4a58623139b867b777b5f9684304115f
s <myae3080@naver.com>
2021-06-07 13:15:53 +0900

    practice1

---
+++ src/practice1/Coffee.java
@@ -0,0 +1,13 @@
+package practice1;
+
+public class Coffee {
+    private final String coffeeBean;
+
+    public Coffee(String coffeeBean) {
+        this.coffeeBean = coffeeBean;
+    }
+
+    public String getCoffeeBean() {
+        return coffeeBean;
+    }
+}
---
+++ src/practice1/Espresso.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public class Espresso extends Coffee {
+
+    public Espresso(String coffeeBean) {
+        super(coffeeBean);
+    }
+}
